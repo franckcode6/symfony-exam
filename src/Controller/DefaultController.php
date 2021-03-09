880ae79bfc346cb718df3e2b0f3470771e495274
franckcode6 <franck.quasevi@yahoo.com>
2021-03-09 15:35:45 +0100

    exo 3

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
+use App\Repository\ContactRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,20 +17,27 @@ class DefaultController extends AbstractController
     /**
      * @Route("/", name="default_index")
      */
-    public function index(): Response
+    public function index(ContactRepository $contactRepository): Response
     {
+        $contacts = $contactRepository->findAll();
         return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
+            'contacts' => $contacts,
         ]);
     }
 
     /**
      * @Route("/contact", name="default_contact")
      */
-    public function contact(): Response
+    public function new(EntityManagerInterface $entityManager): Response
     {
-        return $this->render('default/contact.html.twig', [
-            'controller_name' => 'DefaultController',
-        ]);
+        $contact = new Contact();
+        $contact->setEmail('test@test.com');
+        $contact->setSubject('Ceci est un test');
+        $contact->setMessage('Un message de test, pouvant être long, ou non. Celui-ci ne l\'est pas :) .');
+
+        $entityManager->persist($contact);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('default_index');
     }
 }
